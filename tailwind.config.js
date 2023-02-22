24694ace15939ec2502f8706e5db4efac3de7be7
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2023-02-22 15:59:16 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gptlogo: '#10a37f',
+        gptdarkgray: '#202123',
+        gptgray: '#343541',
+        gptlightgray: '#444654'
+      }
+    }
+  },
+  plugins: []
+}

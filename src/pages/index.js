e535cb856b24207ca9dc339a7cb45336f693b446
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2023-02-22 21:52:51 -0500

    Componente TypingEffect listo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Avatar } from '@/components/Avatar'
 import { PlusIcon } from '@/components/Icon'
+import { TypingEffect } from '@/components/TypingEffect'
 import Head from 'next/head'
 
 function Layout({ children }) {
@@ -28,6 +29,30 @@ function Aside() {
   )
 }
 
+function Message({ ia, message }) {
+  const avatar = ia ? (
+    <img src='https://img.freepik.com/free-icon/robot_318-490792.jpg' />
+  ) : (
+    <img
+      className='rounded-full'
+      alt='foto_usuario'
+      src='https://media.licdn.com/dms/image/D4E03AQHnBaZMcTxwDw/profile-displayphoto-shrink_400_400/0/1669702152451?e=1682553600&v=beta&t=YDlezHBJgZt7nOHz70U9jn4DyGtw2l8RoJvhORl79sU'
+    />
+  )
+
+  const textElement = ia ? <TypingEffect text={message} /> : message
+  return (
+    <div className={ia ? 'bg-gptlightgray' : 'bg-gptgray'}>
+      <article className='flex gap-4 p-4 m-auto max-w-3xl'>
+        <Avatar>
+          {avatar}
+        </Avatar>
+        <p className='flex-1'>{textElement}</p>
+      </article>
+    </div>
+  )
+}
+
 function Chat() {
   const message = [
     {
@@ -47,18 +72,26 @@ function Chat() {
     <div className='flex flex-col h-full flex-1 pl-[260px]'>
       <main>
         {message.map((message) => (
-          <article key={message.id} className='flex gap-4 p-4 m-auto max-w-3xl'>
-            <Avatar>
-              <img
-                alt='foto_usuario'
-                src='https://media.licdn.com/dms/image/D4E03AQHnBaZMcTxwDw/profile-displayphoto-shrink_400_400/0/1669702152451?e=1682553600&v=beta&t=YDlezHBJgZt7nOHz70U9jn4DyGtw2l8RoJvhORl79sU'
-              />
-            </Avatar>
-            <p>{message.message}</p>
-          </article>
+          <Message key={message.id} {...message} />
         ))}
       </main>
+      <ChatForm />
     </div>
+  )
+}
+
+function ChatForm() {
+  return (
+    <section className='absolute bottom-0 w-full left-0 right-0 ml-32'>
+      <form className='flex flex-row max-w-3xl pt-6 m-auto'>
+        <div className='relative flex flex-col flex-grow w-full px-4 py-3 text-white border rounded-md shadow-lg bg-gptlightgray border-gray-900/50'>
+          <textarea className='w-full h-[24px] resize-none bg-transparent m-0 border-0 outline-none' />
+          <button className='absolute p-1 rounded-md bottom-2 right-2 '>
+            Enviar
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
